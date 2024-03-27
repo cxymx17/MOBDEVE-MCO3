dc5b04a2978183faa7337204251dca4a566944f0
alexikyna <alexi_delacruz@dlsu.edu.ph>
2024-03-27 22:50:28 +0800

    Initial commit

--- app/src/main/java/com/mobdeve/s11/lignes/cymbeline/mco3/navbar/BottomNavbarHelper.java
+++ app/src/main/java/com/mobdeve/s11/lignes/cymbeline/mco3/navbar/BottomNavbarHelper.java
@@ -3,10 +3,10 @@ package com.mobdeve.s11.lignes.cymbeline.mco3.navbar;
 import android.app.Activity;
 import android.content.Intent;
 import android.widget.ImageView;
-import android.view.View;
 import com.mobdeve.s11.lignes.cymbeline.mco3.R;
 import com.mobdeve.s11.lignes.cymbeline.mco3.activity.NotificationActivity;
 import com.mobdeve.s11.lignes.cymbeline.mco3.activity.UserProfileActivity;
+import com.mobdeve.s11.lignes.cymbeline.mco3.activity.DashboardActivity; // Import DashboardActivity
 
 public class BottomNavbarHelper {
     public static void setProfileIconClickListener(Activity activity) {
@@ -19,6 +19,13 @@ public class BottomNavbarHelper {
         ImageView notificationIcon = activity.findViewById(R.id.notificationIcon);
         notificationIcon.setOnClickListener(v -> {
             Intent intent = new Intent(activity, NotificationActivity.class);
+            activity.startActivity(intent);
+        });
+
+        // Set click listener for homeIcon
+        ImageView homeIcon = activity.findViewById(R.id.homeIcon);
+        homeIcon.setOnClickListener(v -> {
+            Intent intent = new Intent(activity, DashboardActivity.class);
             activity.startActivity(intent);
         });
     }
